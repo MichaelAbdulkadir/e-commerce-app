35e236c84772cc4931a0332ddc14c06eb7da5bb0
Michael Abdulkadir <michael.abdulkadir@gmail.com>
2021-03-16 17:57:41 +0800

    added home and product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,29 @@
-import Product from './components/Product';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
 	return (
-		<div className='grid-container'>
-			<header className='row'>
-				<div>
-					<a className='brand' href='/'>
-						E-Commerce
-					</a>
-				</div>
-				<div>
-					<a href='/cart'>Cart</a>
-					<a href='/signin'>Sign In</a>
-				</div>
-			</header>
-			<main>
-				<div className='row center'>
-					{data.products.map((product) => (
-						<Product key={product._id} product={product}></Product>
-					))}
-				</div>
-			</main>
-			<footer className='row center'>All Rights Reserved</footer>
-		</div>
+		<BrowserRouter>
+			<div className='grid-container'>
+				<header className='row'>
+					<div>
+						<a className='brand' href='/'>
+							E-Commerce
+						</a>
+					</div>
+					<div>
+						<a href='/cart'>Cart</a>
+						<a href='/signin'>Sign In</a>
+					</div>
+				</header>
+				<main>
+					<Route path='/product/:id' component={ProductScreen}></Route>
+					<Route path='/' component={HomeScreen} exact></Route>
+				</main>
+				<footer className='row center'>All Rights Reserved</footer>
+			</div>
+		</BrowserRouter>
 	);
 }
 
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Product from '../components/Product';
+import data from '../data';
+
+const HomeScreen = () => {
+	return (
+		<div>
+			<div className='row center'>
+				{data.products.map((product) => (
+					<Product key={product._id} product={product}></Product>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default HomeScreen;
