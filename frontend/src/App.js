2963464a50250778bdfaa027a98073a4357c37e6
Michael Abdulkadir <michael.abdulkadir@gmail.com>
2021-03-16 16:35:42 +0800

    share code on github

---
+++ frontend/src/App.js
@@ -0,0 +1,30 @@
+import Product from './components/Product';
+import data from './data';
+
+function App() {
+	return (
+		<div className='grid-container'>
+			<header className='row'>
+				<div>
+					<a className='brand' href='/'>
+						E-Commerce
+					</a>
+				</div>
+				<div>
+					<a href='/cart'>Cart</a>
+					<a href='/signin'>Sign In</a>
+				</div>
+			</header>
+			<main>
+				<div className='row center'>
+					{data.products.map((product) => (
+						<Product key={product._id} product={product}></Product>
+					))}
+				</div>
+			</main>
+			<footer className='row center'>All Rights Reserved</footer>
+		</div>
+	);
+}
+
+export default App;
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const Product = ({ product }) => {
+	return (
+		<div key={product._id} className='card'>
+			<a href={`/product/${product._id}`}>
+				<img className='medium' src={product.image} alt={product.name} />
+			</a>
+			<div className='card-body'>
+				<a href={`/product/${product._id}`}>
+					<h2>{product.name}</h2>
+				</a>
+				<div className='rating'>
+					<span>
+						<i className='fa fa-star'></i>
+					</span>
+					<span>
+						<i className='fa fa-star'></i>
+					</span>
+					<span>
+						<i className='fa fa-star'></i>
+					</span>
+					<span>
+						<i className='fa fa-star'></i>
+					</span>
+					<span>
+						<i className='fa fa-star-o'></i>
+					</span>
+				</div>
+				<div className='price'>${product.price}</div>
+			</div>
+		</div>
+	);
+};
+
+export default Product;
